ebb44a18f35f7605ffd0c6192d0df88f97332f0f
Asik8 <motasembillah84@gmail.com>
2024-06-07 06:31:57 +0600

    3b done

--- Question Solution/Fall 2022/3b.py
+++ Question Solution/Fall 2022/3b.py
@@ -6,5 +6,9 @@ num_list = list(range(100))
 a = np.array(num_list).reshape(10,10)
 print(a)
 
-col_sum = np.sum(a,axis=0)
+col_sum = np.sum(a,axis=0) # axis 0 refers to column
 print(col_sum)
+
+# To know the row sum:
+row_sum = np.sum(a,axis=1)
+print(row_sum)
